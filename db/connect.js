d40a2eb827e9ed6923919113dafd1c35ec0fbf16
m1taylor <m1taylor@ryerson.ca>
2023-02-21 15:22:57 -0500

    added password and username for sql access

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+// Connecting to SQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'frankie464',
+        database: 'CRM_employee_db'
+    },
+    console.log(`Connected to your CRM database.`)
+
+
+);
