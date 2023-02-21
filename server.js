a500450255a6a636928ec0701e586d36e2250fb5
m1taylor <m1taylor@ryerson.ca>
2023-02-21 15:13:57 -0500

    added packages to serverjs

---
+++ server.js
@@ -0,0 +1,4 @@
+//imports packages  
+const inquirer = require('inquirer'); //command line input
+const mysql = require('mysql2'); //connects to mySQL db
+const consoleTable = require('console.table'); //displays data in table on command line
