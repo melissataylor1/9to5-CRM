23d4f883045bb109d227d692505237242a3c1ef2
m1taylor <m1taylor@ryerson.ca>
2023-02-22 16:48:02 -0500

    addedanswers for first question

--- server.js
+++ server.js
@@ -5,5 +5,53 @@ const consoleTable = require('console.table'); //displays data in table on comma
 
 const connection = require('./db/connect'); //SQL access connection
 
-
-
+start()
+// Function to prompt the user for what they would like to do
+function start() {
+    inquirer.prompt(
+      {name: 'options',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]})
+    .then(answer => {
+      switch (answer.toDo) {
+        case 'View all employees':
+          viewAllEmp();
+          break;
+        case 'View all departments':
+          viewAllDepart();
+          break;
+        case 'View all employees by Department':
+          viewEmpByDepart();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 'Add employee':
+          addEmployee();
+          break;
+        case 'Add department':
+          addDepartment();
+          break;
+        case 'Add role':
+          addRole();
+          break;
+        case 'Update employee role':
+          updateEmpRole();
+          break;
+        case 'EXIT':
+          exitApp();
+          break;
+      }
+    });
+};
+    }
