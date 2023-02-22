9923315f81cc3dfac7b8f97209783a50228e8261
m1taylor <m1taylor@ryerson.ca>
2023-02-22 18:34:07 -0500

    created department and role tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,19 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
+/*department table*/
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
+);
+
+/*role table*/
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
 );
