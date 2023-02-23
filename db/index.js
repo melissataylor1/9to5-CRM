8280cea2759f4262757c394572827e1b797a4917
m1taylor <m1taylor@ryerson.ca>
2023-02-22 19:13:40 -0500

    added query for employees view db

--- db/index.js
+++ db/index.js
@@ -4,4 +4,13 @@ class DB{
     constructor(connection){
         this.connection = connection
     }
+    findAllEmployees(){
+        return this.connection.promise().query(
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+            FROM employee 
+            LEFT JOIN role ON employee.role_id = role.id 
+            LEFT JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id;`
+        );
+    }
 }
